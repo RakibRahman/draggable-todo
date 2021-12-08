09c8d4f7478fcfa03127dc5e20bf52a67f3df938
RakibRahman <md.rr.talukder@gmail.com>
2021-12-08 22:13:24 +0600

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
-import InputFeild from "./components/InputFeild";
+import InputField from "./components/InputField";
 import TodoList from "./components/TodoList";
-import { Todo } from "./models/models";
+import { Todo } from "./interface/interface";
+import { Box, Text } from "@chakra-ui/react";
+import { DragDropContext } from "react-beautiful-dnd";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [importantTodos, setImportantTodos] = useState<Todo[]>([]);
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
@@ -18,11 +21,20 @@ const App: React.FC = () => {
   };
 
   return (
-    <div className="App">
-      <span className="heading">Taskify</span>
-      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
-      <TodoList todos={todos} setTodos={setTodos} />
-    </div>
+    <DragDropContext onDragEnd={() => {}}>
+      <Box className="App">
+        <Text as="span" className="heading">
+          Taskify
+        </Text>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          importantTodos={importantTodos}
+          setImportantTodos={setImportantTodos}
+        />
+      </Box>
+    </DragDropContext>
   );
 };
 
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,24 +1,71 @@
 import React from "react";
-import { Todo } from "../models/models";
+import { Todo } from "../interface/interface";
 import SingleTodo from "./SingleTodo";
-
+import { Box, Heading, Flex, Spacer } from "@chakra-ui/react";
+import { Droppable } from "react-beautiful-dnd";
 interface props {
   todos: Todo[];
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+  importantTodos: Todo[];
+  setImportantTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList: React.FC<props> = ({ todos, setTodos }) => {
+const TodoList: React.FC<props> = ({
+  todos,
+  setTodos,
+  importantTodos,
+  setImportantTodos,
+}) => {
   return (
-    <div className="todos">
-      {todos?.map((todo) => (
-        <SingleTodo
-          todos={todos}
-          todo={todo}
-          key={todo.id}
-          setTodos={setTodos}
-        />
-      ))}
-    </div>
+    <Flex w="900px" justify="center" align="center" gridGap="5">
+      <Droppable droppableId="todolist">
+        {(provided) => (
+          <Flex
+            flexDirection="column"
+            className="todos"
+            w="300px"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <Heading> Task List</Heading>
+            {todos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={todos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </Flex>
+        )}
+      </Droppable>
+      <Spacer />
+      <Droppable droppableId="importantTodos">
+        {(provided) => (
+          <Flex
+            flexDirection="column"
+            w="300px"
+            bg="green"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            <Heading>Important Task List</Heading>
+            {importantTodos?.map((todo, index) => (
+              <SingleTodo
+                index={index}
+                todos={importantTodos}
+                todo={todo}
+                key={todo.id}
+                setTodos={setImportantTodos}
+              />
+            ))}
+            {provided.placeholder}
+          </Flex>
+        )}
+      </Droppable>
+    </Flex>
   );
 };
 
